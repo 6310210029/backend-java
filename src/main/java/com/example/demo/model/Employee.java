fa9c719194cc7a475d5a2b13c771b2bb563a101f
DELL DELL <6310210029@psu.ac.th>
2023-08-22 12:01:24 +0700

    join table

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -1,17 +1,21 @@
 package com.example.demo.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="employee")
-
+@Table(name = "employee")
 public class Employee {
-	
-    @Id
+
+	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	private Integer employeeId;
@@ -19,15 +23,13 @@ public class Employee {
 	private String lastName;
 	private Integer salary;
 	
+	@ManyToOne
+	@JoinColumn(name = "role_id")
+	private Role role;
 	
-	
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
+	@OneToMany
+	@JoinColumn(name = "employee_id")
+	private List<Skill> skills;
 
 	public Employee() {
 		super();
@@ -40,31 +42,63 @@ public class Employee {
 		this.lastName = lastName;
 		this.salary = salary;
 	}
+
 	
+	public List<Skill> getSkills() {
+		return skills;
+	}
+
+	public void setSkills(List<Skill> skills) {
+		this.skills = skills;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 	public Integer getEmployeeId() {
 		return employeeId;
 	}
+
 	public void setEmployeeId(Integer employeeId) {
 		this.employeeId = employeeId;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public Integer getSalary() {
 		return salary;
 	}
+
 	public void setSalary(Integer salary) {
 		this.salary = salary;
 	}
-	
-	
+
 }
+
